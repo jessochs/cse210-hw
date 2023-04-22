f198338a091287ece0beb0d71a2a897cddb375f2
jessochs <fac17002@byui.edu>
2023-04-22 11:48:50 -0700

    completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your percent in the class?");
+        string grade = Console.ReadLine();
+
+        int x = int.Parse(grade);
+
+        string letter = "";
+
+        if (x >= 90)
+        {
+            Console.WriteLine("You have an A");
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (x > 70)
+        {
+            Console.WriteLine("You Passed the class");
+        }
+        else{
+            Console.WriteLine("You did not pass the class");
+        }
+        
     }
 }
