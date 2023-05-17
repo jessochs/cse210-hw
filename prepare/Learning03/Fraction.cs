7ada9ff20594fdc469448a7a54e226cae692475d
jessochs <fac17002@byui.edu>
2023-05-17 10:43:46 -0700

    added fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,52 @@
+using System;
+public class Fraction {
+    //I guess you don't have to se the private to anything before?
+            private int _top;
+            private int _bottom;
+
+            public Fraction() {
+                _top = 1;
+                _bottom = 1;
+
+            }
+            public Fraction(int top) {
+                _top = top;
+                _bottom = 1;
+            }
+
+            public Fraction(int top, int bottom) {
+                _top = top;
+                _bottom = bottom;
+            }
+
+//why doesn't the answer code show getters and setters like we're supposed to do?
+
+            public int GetTop()
+            {
+                return _top;
+            }
+
+            public int SetTop(int top)
+            {
+                return _top;
+            }
+
+            public int GetBottom()
+            {
+                return _bottom;
+            }
+
+            public int SetBottom(int bottom)
+            {
+                return _bottom;
+            }
+            public string GetFractionString(){
+                string wholeFraction = $"{_top}/{_bottom}";
+            }
+            public double GetDecimalValue(){
+
+                return (double)_top / (double)_bottom;
+
+            }
+
+        }
