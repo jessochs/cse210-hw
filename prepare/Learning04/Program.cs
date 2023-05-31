e6401b757466165993dee3a67d0879ce14c98187
jessochs <fac17002@byui.edu>
2023-05-30 18:47:01 -0700

    learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment();
+        assignment1.SetName("Samuel Jackson");
+        assignment1.SetTopic("multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment();
+        assignment2.SetName("jessica");
+        assignment2.SetTopic("English");
+        assignment2.SetSection("1-2");
+        assignment2.SetProblems("10-20, 34-37");
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment();
+        assignment3.SetName("Mary Waters");
+        assignment3.SetTopic("European History");
+        assignment3.SetTitle("The causes of world war 11");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWRitingAssignment());
+
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _title = "";
+
+    public string GetTitle()
+{
+    return _title;
+}
+
+public void SetTitle(string title)
+{
+    _title = title;
+}
+public string GetWRitingAssignment()
+{
+    return $"{_title}";
+}
+
+
+}
+
